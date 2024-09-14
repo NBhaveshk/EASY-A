65d790ef974590db9dd2c5f817a5f980d5a73af7
Yeo Kay Hong <yeokayhong@gmail.com>
2024-09-14 16:34:58 +0800

    Implement modal popup for posts

--- easya/src/Context.js
+++ easya/src/Context.js
@@ -1,17 +1,31 @@
 import React, { useContext, useState } from 'react';
+import PostModal from './components/PostModal';
 
 const AuthContext = React.createContext()
+const PostModalContext = React.createContext()
 
 export function useAuth() {
     return useContext(AuthContext)
 }
 
+export function usePostModal() {
+    return useContext(PostModalContext)
+}
+
 export default function Context({ children }) {
-    const [wallet_id, set_wallet_id] = useState(null);
+    const [wallet_id, set_wallet_id] = useState("REMOVE THIS");
+    const [post_modal_data, set_post_modal_data] = useState(null);
+
+    function clear_post_modal_data() {
+        set_post_modal_data()
+    }
 
     return (
         <AuthContext.Provider value={{ wallet_id, set_wallet_id }}>
-            {children}
+            <PostModalContext.Provider value={{ set_post_modal_data, clear_post_modal_data }}>
+                {post_modal_data && <PostModal data={post_modal_data} />}
+                {children}
+            </PostModalContext.Provider>
         </AuthContext.Provider>
     );
 }
--- easya/src/pages/Community.js
+++ easya/src/pages/Community.js
@@ -1,8 +1,12 @@
+import { usePostModal } from "../Context"
+
 export default function Community() {
+    const { set_post_modal_data } = usePostModal()
     return (
         <main>
             <div>
                 <h1>Community</h1>
+                <button onClick={() => set_post_modal_data({ id: 1, author: "author", date: "date", cover_image: <img src="https://picsum.photos/400/400" alt="random post" /> })}>Open Post Modal</button>
             </div>
         </main>
     )
