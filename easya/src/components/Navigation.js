4df1da4dab1a6f5ab8e1359cc4f2191b6c0fd128
Yeo Kay Hong <yeokayhong@gmail.com>
2024-09-14 14:41:33 +0800

    Link navbar items to pages

--- easya/src/components/Navigation.js
+++ easya/src/components/Navigation.js
@@ -1,12 +1,14 @@
 import styles from './Navigation.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Header({ className }) {
     return (
         <nav className={styles.navigation}>
             <div className={styles.options}>
-                <div>Posts</div>
-                <div>Create</div>
-                <div>Notifications</div>
+                <Link to="/">Posts</Link>
+                <Link to="/create">Create</Link>
+                <Link to="/notifications">Notifications</Link>
+                <Link to="/login">Login</Link>
             </div>
         </nav>
     )
