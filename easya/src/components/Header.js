ed0076de97a664bf098fa6a3d6c23ec80f19d31c
Yeo Kay Hong <yeokayhong@gmail.com>
2024-09-14 22:12:28 +0800

    Create logout button

--- easya/src/components/Header.js
+++ easya/src/components/Header.js
@@ -1,12 +1,28 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './Header.module.css'
 import { useAuth } from '../Context'
+import { ReactComponent as Icon } from '../assets/icons/bars.svg';
 
 export default function Header() {
-    const { wallet_id } = useAuth()
+    const { wallet_id, logout } = useAuth()
+    const navigate = useNavigate()
+
+    function handle_logout() {
+        logout()
+        navigate('/')
+    }
+
     return (
         <header className={styles.header}>
             <h1>Fashion Fiesta</h1>
-            <p>Wallet ID: {wallet_id}</p>
+            <div className={styles.menu}>
+                <input type="checkbox" />
+                <Icon className={styles.icon} />
+                <div className={styles.dropdown}>
+                    <p>{wallet_id.slice(0, 16)}</p>
+                    <button onClick={handle_logout}>Logout</button>
+                </div>
+            </div>
         </header>
     )
 }
